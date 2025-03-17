a0f7134407b34ec202a70e29b18d3da023f9e6e2
Saxton Morby <saxto@DESKTOP-Q9UU58M.(none)>
2025-03-17 16:05:51 -0600

    Create ConfigureDatabase class to remove duplicate code in MySqlDAO's

---
+++ server/src/main/java/dataaccess/ConfigureDatabase.java
@@ -0,0 +1,35 @@
+package dataaccess;
+
+import java.sql.SQLException;
+
+public class ConfigureDatabase {
+
+    public ConfigureDatabase() {}
+
+    public static void run(String[] statements) throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : statements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
+    public static void executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                }
+                ps.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to access database: %s", ex.getMessage()));
+        }
+    }
+}
