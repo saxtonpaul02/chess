81411e5ec1440ca647efb3c649ded597b545ba3a
Saxton Morby <saxto@DESKTOP-Q9UU58M.(none)>
2025-03-17 22:56:05 -0600

    Update MySqlGameDAO.executeUpdate() to have {} on if statements.

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -117,10 +117,10 @@ public class MySqlGameDAO implements GameDAO {
             try (var ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
                 for (var i = 0; i < params.length; i++) {
                     var param = params[i];
-                    if (param instanceof String p) ps.setString(i + 1, p);
-                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
-                    else if (param instanceof ChessGame p) ps.setString(i + 1, p.toString());
-                    else if (param == null) ps.setNull(i + 1, NULL);
+                    if (param instanceof String p) {ps.setString(i + 1, p);}
+                    else if (param instanceof Integer p) {ps.setInt(i + 1, p);}
+                    else if (param instanceof ChessGame p) {ps.setString(i + 1, p.toString());}
+                    else if (param == null) {ps.setNull(i + 1, NULL);}
                     }
                 ps.executeUpdate();
                 var rs = ps.getGeneratedKeys();
