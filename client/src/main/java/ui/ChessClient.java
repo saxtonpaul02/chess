1ddfc0ccf3747ecdc4fd3a20ec18c7648ccb67ef
Saxton Morby <saxto@DESKTOP-Q9UU58M.(none)>
2025-03-24 18:49:24 -0600

    Initial creation of ChessClient.java and ServerFacade.java

---
+++ client/src/main/java/ui/ChessClient.java
@@ -0,0 +1,121 @@
+package ui;
+
+import chess.ChessGame;
+import server.ServerFacade;
+import java.util.Arrays;
+
+public class ChessClient {
+    private String visitorName = null;
+    private final ServerFacade server;
+    private final String serverUrl;
+    private State state = State.LOGGED_OUT;
+
+    public ChessClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "create" -> createGame(params);
+                case "list" -> listGames();
+                case "join" -> playGame(params);
+                case "observe" -> observeGame(params);
+                case "logout" -> logout();
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String register(String... params) throws Exception {
+        if (params.length == 3) {
+            server.register(params);
+            state = State.LOGGED_IN;
+            visitorName = params[0];
+            return String.format("Logged in as %s.", visitorName);
+        }
+        throw new Exception("Error registering, please try again.");
+    }
+
+    public String login(String... params) throws Exception {
+        if (params.length == 2) {
+            server.login(params);
+            state = State.LOGGED_IN;
+            visitorName = params[0];
+            return String.format("Logged in as %s.", visitorName);
+        }
+        throw new Exception("Error logging in, please try again.");
+    }
+
+    public String createGame(String... params) throws Exception {
+        assertLoggedIn();
+        if (params.length == 2) {
+            server.createGame(params);
+            return String.format("Successfully created game %s.", params[0]);
+        }
+        throw new Exception("Error creating game, please try again.");
+    }
+
+    public String listGames() throws Exception {
+        assertLoggedIn();
+        throw new Exception("Error listing games, please try again.");
+    }
+
+    public String playGame(String... params) throws Exception {
+        assertLoggedIn();
+        throw new Exception("Error joining game, please try again.");
+    }
+
+    public String observeGame(String... params) throws Exception {
+        assertLoggedIn();
+        throw new Exception("Error getting game, please try again.");
+    }
+
+    public String logout() throws Exception {
+        assertLoggedIn();
+        try {
+            server.logout();
+            state = State.LOGGED_OUT;
+            visitorName = null;
+            return "Successfully logged out.";
+        } catch (Exception ex) {
+            throw new Exception("Error logging out, please try again");
+        }
+    }
+
+    public String help() {
+        if (state == State.LOGGED_OUT) {
+            return """
+                    register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                    login <USERNAME> <PASSWORD> - to play chess
+                    quit - playing chess
+                    help - with possible commands
+                    """;
+        } else {
+            return """
+                    create <NAME> - a game
+                    list - games
+                    join <ID> [WHITE|BLACK] - a game
+                    observe <ID> - a game
+                    logout - when you are done
+                    quit - playing chess
+                    help - with possible commands
+                    """;
+        }
+    }
+
+    private void assertLoggedIn() throws Exception {
+        if (state == State.LOGGED_OUT) {
+            throw new Exception("You must log in");
+        }
+    }
+}
---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,71 @@
+package server;
+
+import chess.ChessGame;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.*;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public void register(String... params) throws Exception {
+        var path = "/user";
+        this.makeRequest("POST", path, params,null);
+    }
+
+    public void login(String... params) throws Exception {
+        var path = "/session";
+        this.makeRequest("POST", path, params, null);
+    }
+
+    public ChessGame createGame(String... params) throws Exception {
+        var path = "/game";
+        return this.makeRequest("POST", path, params, ChessGame.class);
+    }
+
+    public void logout() throws Exception {
+        var path = "/session";
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws Exception {
+        URL url = (new URI(serverUrl + path)).toURL();
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod(method);
+        http.setDoOutput(true);
+        writeBody(request, http);
+        http.connect();
+        return readBody(http, responseClass);
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content_Type", "application/json");
+            String requestData = new Gson().toJson(request);
+            try (OutputStream requestBody = http.getOutputStream()) {
+                requestBody.write(requestData.getBytes());
+            }
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream responseBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(responseBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+}
