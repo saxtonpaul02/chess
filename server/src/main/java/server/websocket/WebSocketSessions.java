d2c862ee91dabf68d257477e20bfd8838bb41ad7
Saxton Morby <saxto@DESKTOP-Q9UU58M.(none)>
2025-04-08 23:50:45 -0600

    Fixed WebSocketSessions to handle when gameID is not yet in the map

--- server/src/main/java/server/websocket/WebSocketSessions.java
+++ server/src/main/java/server/websocket/WebSocketSessions.java
@@ -8,7 +8,15 @@ public class WebSocketSessions {
 
     public final HashMap<Integer, HashSet<Session>> sessionMap = new HashMap<>();
 
-    public void add(int gameID, Session session) { sessionMap.get(gameID).add(session); }
+    public void add(int gameID, Session session) {
+        if (get(gameID) != null) {
+            sessionMap.get(gameID).add(session);
+        } else {
+            HashSet<Session> sessionSet = new HashSet<>();
+            sessionSet.add(session);
+            sessionMap.put(gameID, sessionSet);
+        }
+    }
 
     public void remove(int gameID, Session session) { sessionMap.get(gameID).remove(session); }
 
